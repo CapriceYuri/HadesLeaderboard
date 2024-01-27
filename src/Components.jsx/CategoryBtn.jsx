0cc7046e6a3ac331497ce801ff84c3bbae75541e
Caprice Yuri <123914679+CapriceYuri@users.noreply.github.com>
2024-01-27 13:20:58 -0500

    Adjust grid-cols

--- src/Components.jsx/CategoryBtn.jsx
+++ src/Components.jsx/CategoryBtn.jsx
@@ -24,7 +24,7 @@ import { Button } from "@material-tailwind/react";
 
 export default function CategoryButtons({ onClick }) {
   return (
-    <div className="grid grid-cols-4 place-items-center gap-2 p-4">
+    <div className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 place-items-center gap-2 p-4">
       {categories.map((cate, index) => (
         <Button
           onClick={() => onClick(index + 4)}
--- src/Components.jsx/WeaponBtn.jsx
+++ src/Components.jsx/WeaponBtn.jsx
@@ -4,7 +4,7 @@ import { Button } from "@material-tailwind/react";
 
 export default function CategoryWeapBtns({ onClick }) {
   return (
-    <div className="grid grid-cols-4 place-items-center gap-2 p-4">
+    <div className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 place-items-center gap-2 p-4">
       {weapons.map((weapon, index) => (
         <Button
           onClick={() => onClick(index + 23)}
