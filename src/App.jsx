25a96fe601e3cfc437f28129af179d48e3af1cf6
Caprice Yuri <123914679+CapriceYuri@users.noreply.github.com>
2024-02-01 00:20:59 -0500

    remove Vercel-Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import MainContent from "./MainContent";
-import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <>
       <MainContent />
-      <Analytics />
     </>
   );
 }
