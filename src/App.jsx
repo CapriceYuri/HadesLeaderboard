cab7f2b04bd50ee352666ce4b1fded389a314bf7
Caprice Yuri <123914679+CapriceYuri@users.noreply.github.com>
2024-02-01 00:11:47 -0500

    Added Vercel-Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import { Analytics } from "@vercel/analytics/react";
+
 import MainContent from "./MainContent";
 
 function App() {
   return (
     <>
       <MainContent />
+      <Analytics />
     </>
   );
 }
