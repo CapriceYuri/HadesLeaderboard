2b87ce2089abfdccea3ac61b0fc2799bc1431480
Caprice Yuri <123914679+CapriceYuri@users.noreply.github.com>
2024-02-02 20:14:45 -0500

    added heat diff

--- src/Components.jsx/RecordSummary2.jsx
+++ src/Components.jsx/RecordSummary2.jsx
@@ -40,7 +40,13 @@ export default function RecordSummaryCard() {
                 size="xl"
               />
               <Avatar
-                src={`rings/black.gif`}
+                src={
+                  run.heat < 60
+                    ? `rings/green-card.gif`
+                    : run.heat === 64
+                    ? `rings/purple-card.gif`
+                    : `rings/blue-card.gif`
+                }
                 className="absolute top-0 start-50 transform -translate-x-full"
                 size="xl"
               />
@@ -102,26 +108,6 @@ export default function RecordSummaryCard() {
           </Card>
         ))
       )}
-      {/* TESTING */}
-      {/* <Card className="bg-transparent backdrop-blur-sm">
-        <CardHeader floated={false}>
-          <img
-            src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"
-            alt="card-image"
-            className="object-cover"
-          />
-        </CardHeader>
-        <CardBody>
-          <Typography variant="h5" color="white" className="mb-2">
-            UI/UX Review Check
-          </Typography>
-          <Typography variant="small" color="white">
-            The place is close to Barceloneta Beach and bus stop just 2 min by
-            walk and near to &quot;Naviglio&quot; where you can enjoy the main
-            night life in Barcelona.
-          </Typography>
-        </CardBody>
-      </Card> */}
     </div>
   );
 }
